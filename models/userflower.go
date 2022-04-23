a0e262eda62bf8134a20cddee7dba2d2d11061dc
lsdrfrx <33656776+lsdrfrx@users.noreply.github.com>
2022-04-23 15:59:51 +0300

    Minor changes

--- models/userflower.go
+++ models/userflower.go
@@ -4,7 +4,7 @@ type UserFlower struct {
 	ID               int    `json:"-"`
 	NameNomenclature string `json:"name_nomenclature"`
 	FlowerID         string `json:"id"`
-	OwnerID          string `json:"-"`
+	OwnerID          string `json:"owner_id"`
 	Name             string `json:"name"`
 	Alive            bool   `json:"alive"`
 	NeedWater        bool   `json:"need_water"`
